c466388ee41a39482a1c6342460f4a296b043f34
chaonanwang <chaonan.wang@zdh.co.jp>
2024-04-11 13:50:07 +0900

    added

--- utils/utility_funcs.py
+++ utils/utility_funcs.py
@@ -1,9 +1,20 @@
 import requests
 
-from notification_settings import (
-    MAIN_NOTIFICATION_TEAM_URL,
-    MENTION_MEMBERS,
+# from notification_settings import (
+#     MAIN_NOTIFICATION_TEAM_URL,
+#     MENTION_MEMBERS,
+# )
+# JOB成功・失敗を通知するWebhook URL
+MAIN_NOTIFICATION_WEBHOOK_URL = (
+    'https://avantg.webhook.office.com/webhookb2'
+    '/87c2db7e-f692-446f-86fd-2d5817ac89af@c120f892-e83d-4d2d-b663-cd7a76efc4ea'
+    '/IncomingWebhook/9541568aa1624155a8b451aa3fecf6a0/5607e0eb-cc9a-43f7-ae9a-7f64075cb93e'
 )
+
+# JOB失敗の際にメンションするメンバー一覧
+MENTION_MEMBERS = [
+    'chaonan.wang@avantcorp.com',
+]
 
 
 def _prep_job_error_message(job_name):
