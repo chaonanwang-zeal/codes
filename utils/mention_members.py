0741d70cff210f29a68a2a118dbd737f1d0023f7
chaonanwang <chaonan.wang@zdh.co.jp>
2024-04-11 14:50:39 +0900

    added

--- utils/mention_members.py
+++ utils/mention_members.py
@@ -1,6 +1,7 @@
 MENTION_MEMBERS = [
     'chaonan.wang@avantcorp.com',
+    'masayuki.fujiwara@avantcorp.com'
 ]
 
 if __name__ == "__main__":
-    print("[{}]".format(", ".join("'" + member + "'" for member in MENTION_MEMBERS)))
+    print([m for m in MENTION_MEMBERS])
