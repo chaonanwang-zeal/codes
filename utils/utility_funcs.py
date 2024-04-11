52600e109bcf70c6e8cfbf0deead7e919492ede6
chaonanwang <chaonan.wang@zdh.co.jp>
2024-04-11 14:28:34 +0900

    added

--- utils/utility_funcs.py
+++ utils/utility_funcs.py
@@ -1,38 +1,20 @@
 import requests
-
-# from notification_settings import (
-#     MAIN_NOTIFICATION_TEAM_URL,
-#     MENTION_MEMBERS,
-# )
-# JOB成功・失敗を通知するWebhook URL
-MAIN_NOTIFICATION_WEBHOOK_URL = (
-    'https://avantg.webhook.office.com/webhookb2'
-    '/87c2db7e-f692-446f-86fd-2d5817ac89af@c120f892-e83d-4d2d-b663-cd7a76efc4ea'
-    '/IncomingWebhook/9541568aa1624155a8b451aa3fecf6a0/5607e0eb-cc9a-43f7-ae9a-7f64075cb93e'
-)
-
-# JOB失敗の際にメンションするメンバー一覧
-MENTION_MEMBERS = [
-    'chaonan.wang@avantcorp.com',
-]
+import sys
 
 
-def _prep_job_error_message(job_name):
+def _prep_job_error_message(mention_members):
     """
     JOBが失敗した場合の通知メッセージを作成する
     失敗した場合は運用メンバーへメンションする
-
-    Args:
-        job_name (str): 通知するJOB名
 
     Returns:
         dict: 通知メッセージ
     """
 
-    notification_msg = f'【{job_name}】 が失敗しました！！！'
+    notification_msg = 'GCSへのデプロイ作業が失敗しました！Githubでご確認ください！'
 
     entries = []
-    for m in MENTION_MEMBERS:
+    for m in mention_members:
         entries.append(
             {
                 'type': 'mention',
@@ -43,7 +25,7 @@ def _prep_job_error_message(job_name):
                 },
             }
         )
-    mentions = ''.join([f'<at>{m}</at> ' for m in MENTION_MEMBERS])
+    mentions = ''.join([f'<at>{m}</at> ' for m in mention_members])
 
     message = {
         'type': 'message',
@@ -67,7 +49,8 @@ def _prep_job_error_message(job_name):
 
     return message
 
-def notify_message(message, webhook_url):
+
+def notify_message(webhook_url, mention_members):
     """
     メッセージをteamsへ通知する
 
@@ -76,23 +59,19 @@ def notify_message(message, webhook_url):
         webhook_url (string): teamsへの通知に利用するwebhookのURL
     """
     response = requests.post(
-        webhook_url,
-        json=message,
+        webhook_url=webhook_url,
+        json=_prep_job_error_message(mention_members),
     )
     print(f'response: {response}')
 
-def notify_job_error(job_name, webhook_url=MAIN_NOTIFICATION_WEBHOOK_URL):
-    """
-    JOBが失敗した場合の通知メッセージを作成する
-    失敗した場合は運用メンバーへメンションする
 
-    Args:
-        job_name (str): 通知するJOB名
-        webhook_url (string): teamsへの通知に利用するwebhookのURL
-    """
+if __name__ == "__main__":
 
-    notify_message(_prep_job_error_message(job_name), webhook_url)
+    # パラメータ解析
+    webhook_url = sys.argv[1]
+    mention_members = sys.argv[2]
 
+    # 関数実行
+    notify_message(webhook_url, mention_members)
 
-notify_job_error('GCSへのデプロイ作業')
 
